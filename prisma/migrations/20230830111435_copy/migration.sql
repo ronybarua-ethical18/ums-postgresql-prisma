e3bb82614432e988ae82da0863a72da6e77b278b
Rony <ronybarua-ethical18>
2023-08-30 17:14:56 +0600

    new file added

---
+++ prisma/migrations/20230830111435_copy/migration.sql
@@ -0,0 +1,72 @@
+/*
+  Warnings:
+
+  - You are about to drop the `CourseToPrerequisite` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `buildings` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `course_faculties` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `courses` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `offered_course_sections` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `offered_courses` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `rooms` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `semester_registrations` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "CourseToPrerequisite" DROP CONSTRAINT "CourseToPrerequisite_courseId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CourseToPrerequisite" DROP CONSTRAINT "CourseToPrerequisite_preRequisiteId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "course_faculties" DROP CONSTRAINT "course_faculties_courseId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "course_faculties" DROP CONSTRAINT "course_faculties_facultyId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "offered_course_sections" DROP CONSTRAINT "offered_course_sections_offeredCourseId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "offered_course_sections" DROP CONSTRAINT "offered_course_sections_semesterRegistrationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "offered_courses" DROP CONSTRAINT "offered_courses_academicDepartmentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "offered_courses" DROP CONSTRAINT "offered_courses_courseId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "offered_courses" DROP CONSTRAINT "offered_courses_semesterRegistrationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "rooms" DROP CONSTRAINT "rooms_buildingId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "semester_registrations" DROP CONSTRAINT "semester_registrations_academicSemesterId_fkey";
+
+-- DropTable
+DROP TABLE "CourseToPrerequisite";
+
+-- DropTable
+DROP TABLE "buildings";
+
+-- DropTable
+DROP TABLE "course_faculties";
+
+-- DropTable
+DROP TABLE "courses";
+
+-- DropTable
+DROP TABLE "offered_course_sections";
+
+-- DropTable
+DROP TABLE "offered_courses";
+
+-- DropTable
+DROP TABLE "rooms";
+
+-- DropTable
+DROP TABLE "semester_registrations";
+
+-- DropEnum
+DROP TYPE "SemesterRegistrationStatus";
